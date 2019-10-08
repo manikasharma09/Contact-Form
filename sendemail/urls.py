62be738b6e47febbfc0ff6cf06e8f0293c1d0998
Manika Sharma <51908554+manikasharma09@users.noreply.github.com>
2019-10-08 15:07:46 +0530

    Add files via upload

---
+++ sendemail/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path,include
+from .views import emailView,successView
+
+urlpatterns=[
+    path('email/',emailView,name='email'),
+    path('success/',successView,name='success'),
+
+]
